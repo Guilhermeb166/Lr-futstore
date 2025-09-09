45a6976b4ebe5886058bb7a9caa86c66f0463c0f
Guilherme <guilhermebarroso166@gmail.com>
2025-09-08 22:57:45 -0300

    Reapply "mudança no footer"
    This reverts commit 4d254775062e79c893bfc4440665a23f16211e73.

--- src/Layout/footer/footer.jsx
+++ src/Layout/footer/footer.jsx
@@ -1,5 +1,4 @@
-import { FaPhoneFlip } from 'react-icons/fa6'
-import { MdOutlineMail } from "react-icons/md";
+import { FaInstagram } from 'react-icons/fa6'
 import { useNavigate } from 'react-router-dom';
 import styles from './footer.module.css'
 export default function Footer(){
@@ -16,9 +15,8 @@ export default function Footer(){
                 </ul>
                 <span></span>
                 <ul className={styles.listFooter}>
-                    <strong>Contato</strong>
-                    <li><FaPhoneFlip /> 85-00000-0000</li>
-                    <li><MdOutlineMail/> lrfutstore@gmail.com  </li>
+                    <FaInstagram className={styles.iconInsta}/>
+                    <li>lrfutstore_</li>
                 </ul>
             </div>
             <p className={styles.copy}>&copy;  FUT STORE 2025 - Todos os direitos reservados. Conheça nossa política de privacidade</p>
